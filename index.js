a020d41335d9fc2e3c628f81b99243031810ba3e
JaredBoehm <me@jaredboehm.com>
2023-09-04 16:23:29 -0600

    finalized version

--- index.js
+++ index.js
@@ -1,13 +1,72 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+const gm = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Repo Title:',
+    },
+    {
+        type: 'confirm',
+        name: 'includeWatermark',
+        message: 'Would you like to include the watermark?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Repo Description:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation Instructions:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage Details:',
+    },
+    {
+        type: 'list',
+        message: 'License:',
+        name: 'license',
+        choices: ['MIT', 'ISC', 'GNU 3.0', 'GNU 2.0', 'Apache 2.0', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Contribution Instructions:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Testing Details:',
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'Github Username:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Email:',
+    },
+    {
+        type: 'input',
+        name: 'contactInstructions',
+        message: 'Contact Instructions:',
+    }
+]
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+inquirer
+    .prompt(questions)
+    .then((data) => {
+        fs.writeFile('README.md', gm(data), (err) => {
+            err ? console.log(err) : console.log('README Generated!')
+        })
+    })
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,88 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// create the link to the license, if none return an empty string
+function getLicenseLink(license) {
+    let licenseLink = `https://spdx.org/licenses/`
+    switch (license) {
+        case 'MIT':
+            licenseLink += `MIT.html`
+            break
+        case 'ISC':
+            licenseLink += `ISC.html`
+            break
+        case 'GNU 3.0':
+            licenseLink += `GPL-3.0-or-later.html`
+            break
+        case 'GNU 2.0':
+            licenseLink += `GPL-2.0-or-later.html`
+            break
+        case 'Apache 2.0':
+            licenseLink += `Apache-2.0.html`
+            break
+        default:
+            return ''
+    }
+    return licenseLink
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+    let licenseLink = getLicenseLink(license)
+    if (licenseLink === '') { // this will not render a badge if there is no license
+        return licenseLink 
+    }
+    return `[![${license} License Badge](https://img.shields.io/badge/LICENSE-${license}-blue)](${licenseLink})`
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license, title) {
+    let licenseLink = getLicenseLink(license)
+    if (licenseLink === '') { // this will render NA if there is no license
+        return 'NA'
+    }
+    return `${title} is released under the terms of the [${license} License](${licenseLink})`
+}
 
-// TODO: Create a function to generate markdown for README
+// generate all markdown for README file
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+    let markdown =
+`# ${data.title}
+
+${(data.includeWatermark === true) ? 'Generated with [readme-generator](https://github.com/JaredBoehm/readme-generator)' : ''}
+
+## Description
+${renderLicenseBadge(data.license)}
+
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#Contributing)
+- [Tests](#Tests)
+- [Questions](#Questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${renderLicenseSection(data.license, data.title)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+My Github: [${data.githubUsername}](https://github.com/${data.githubUsername})
+
+My Email: ${data.email}
+
+${data.contactInstructions}`
+
+    return markdown
 }
 
 module.exports = generateMarkdown;
